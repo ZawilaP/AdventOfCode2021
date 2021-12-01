bcdcb0d22de9a2e403fee6f6e4343d8ee27d5e81
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2021-12-01 19:50:08 +0100

    Add files via upload

---
+++ day1.scala
@@ -0,0 +1,47 @@
+import scala.annotation.tailrec
+import scala.io.Source._
+
+object day1 extends App {
+  def countDepth(measurements: Seq[Int]): Int = {
+    @tailrec
+    def helper(previousVal: Int, acc: Int, measurements: Seq[Int]): Int = {
+      if (measurements.isEmpty) acc
+      else if (measurements.head > previousVal) helper(measurements.head, acc + 1, measurements.tail)
+      else helper(measurements.head, acc, measurements.tail)
+    }
+
+    helper(measurements.head, 0, measurements.tail)
+  }
+
+  val source = fromFile("/Users/piotrzawila-niedzwiecki/IdeaProjects/advent_of_code/data/day1_input.txt")
+  val inputData = source.getLines.toSeq.map(_.toInt)
+
+  println(inputData.mkString(", "))
+
+  val depth = countDepth(inputData)
+
+  println(depth)
+
+  def createWindows(measurements: Seq[Int]): Seq[Int] = {
+    @tailrec
+    def helper(firstVal: Int, secondVal: Int, thirdVal: Int, acc: Seq[Int], measurements: Seq[Int]): Seq[Int] = {
+      if (measurements.isEmpty) acc :+ (firstVal+secondVal+thirdVal)
+      else helper(secondVal, thirdVal, measurements.head, acc :+ (firstVal+secondVal+thirdVal), measurements.tail)
+    }
+
+    helper(measurements.head, measurements.tail.head, measurements.tail.tail.head, Seq.empty, measurements.tail.tail.tail)
+  }
+
+  val windowed = createWindows(inputData)
+
+  println(windowed.mkString(", "))
+
+  println(inputData.length)
+  println(windowed.length)
+
+  val windowedDepth = countDepth(windowed)
+
+  println(windowedDepth)
+
+  source.close()
+}
