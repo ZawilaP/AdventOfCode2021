f8c87dcd5de37e01c58f61c7254f9b1f04d4e61b
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2021-12-07 15:45:58 +0100

    Add files via upload

---
+++ scala/day4.scala
@@ -0,0 +1,85 @@
+import scala.annotation.tailrec
+import scala.io.Source.fromFile
+
+object day4 extends App {
+
+  val boardSize = 5
+
+  case class Field(row: Int, col: Int)
+
+  case class Board(visited: Set[Field], notVisited: Map[Int, Field]) {
+    def wins: Boolean = {
+      val rows = visited.groupBy(_.row).exists(_._2.size == 5)
+      val cols = visited.groupBy(_.col).exists(_._2.size == 5)
+      rows || cols
+    }
+
+    def markNumber(number: Int): Board = {
+      if (notVisited.contains(number)) {
+        Board(visited + notVisited(number), notVisited - number)
+      } else this
+    }
+
+    def calculateSumOfNotVisited: Int = {
+      notVisited.keys.sum
+    }
+  }
+
+  object BoardParser {
+    private def parseNumbers(input: String): List[Int] = {
+      input.split(",").map(_.toInt).toList
+    }
+
+    private def parseBoards(input: List[String]): List[Board] = {
+      val boards = input.grouped(5).toList
+      val parsedNumbers = boards.map(_.flatMap(_.split(" ").filter(_ != "").toList))
+      val fullyParsed = parsedNumbers.map(x => x.zipWithIndex).map(x => x.flatMap(y => Map(y._1.toInt -> Field(y._2 / boardSize, y._2 % boardSize))))
+      fullyParsed.map(x => Board(Set.empty[Field], x.toMap))
+    }
+
+    def parseInput(path: String): (List[Int], List[Board]) = {
+      val source = fromFile(path)
+      val nonEmptyLines = source.getLines().filter(_.nonEmpty).toList
+
+      val numbers = parseNumbers(nonEmptyLines.head)
+      val boards = parseBoards(nonEmptyLines.tail)
+      source.close
+      (numbers, boards)
+    }
+  }
+
+  private def part1(numbers: List[Int], boards: List[Board]): Int = {
+    @tailrec
+    def helper(numbers: List[Int], boards: List[Board], lastCalled: Int): Int = {
+      if (boards.exists(_.wins)) boards.filter(_.wins).map(_.calculateSumOfNotVisited * lastCalled).head
+      else {
+        helper(numbers.tail, boards.map(_.markNumber(numbers.head)), numbers.head)
+      }
+    }
+
+    helper(numbers, boards, 0)
+  }
+
+  private def part2(numbers: List[Int], boards: List[Board]): Int = {
+    @tailrec
+    def helper(numbers: List[Int], boards: List[Board]): Int = {
+      if (boards.length == 1) part1(numbers, boards)
+      else {
+        helper(numbers.tail, boards.map(_.markNumber(numbers.head)).filter(!_.wins))
+      }
+    }
+
+    helper(numbers, boards)
+  }
+
+  import BoardParser.parseInput
+
+  val path = "/Users/piotrzawila-niedzwiecki/IdeaProjects/advent_of_code/data/day4_input.txt"
+
+  val (numbers, boards) = parseInput(path)
+  val firstWinning = part1(numbers, boards)
+  val lastWinning = part2(numbers, boards)
+
+  println(firstWinning)
+  println(lastWinning)
+}
