b01a961c302bfb420c3c3d23f376f4d06215fce2
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2021-12-11 13:42:30 +0100

    Add files via upload

---
+++ scala/day11.scala
@@ -0,0 +1,79 @@
+import scala.annotation.tailrec
+import scala.io.Source.fromFile
+
+object day11 extends Exercise {
+
+  private def parseInput: Fields = {
+    val source = fromFile(path)
+    val input = source.getLines().toList
+    val fields = {
+      for {
+        (line, row) <- input.zipWithIndex
+        (height, col) <- line.zipWithIndex
+      } yield Field(row, col) -> height.asDigit
+    }.toMap
+    fields
+  }
+
+  type Fields = Map[Field, Int]
+
+  object Fields {
+    @tailrec
+    private def findFlashes(fields: Fields, flashes: Set[Field]): Set[Field] = {
+      val updatedFlashes = flashes.flatMap(_.neighbours.toSet)
+        .filter(candidate => fields.get(candidate)
+          .exists(_ + candidate.neighbours.toSet.intersect(flashes).size > 9)) ++ flashes
+      if (updatedFlashes == flashes) flashes else findFlashes(fields, updatedFlashes)
+    }
+
+    private def getNextFields(fields: Fields): Fields = {
+      val incremented = fields.view.mapValues(_ + 1).toMap
+      val flashes = findFlashes(incremented, incremented.filter(_._2 > 9).keySet)
+      incremented.map { case (point, value) =>
+        (point,
+          if (flashes.contains(point)) 0
+          else value + point.neighbours.toSet.count(flashes.contains))
+      }
+    }
+
+    @tailrec
+    def computeFields(fields: Fields, count: Int, acc: List[Fields]): List[Fields] = {
+        if (count == 0) acc
+        else computeFields(fields, count - 1, acc.prepended(getNextFields(acc.head)))
+    }
+
+    @tailrec
+    def indexWhenAllFlash(fields: Fields, steps: Int, acc: List[Fields]): Int = {
+        if (acc.head.values.forall(_ == 0)) steps
+        else {
+          indexWhenAllFlash(fields, steps + 1, acc.prepended(getNextFields(acc.head)))
+        }
+    }
+  }
+
+  case class Neighbours(left: Field, right: Field, above: Field, below: Field, leftUp: Field, leftDown: Field, rightUp: Field, rightDown: Field) {
+    def toSet = Set(left, right, above, below, leftUp, leftDown, rightUp, rightDown)
+  }
+
+  case class Field(row: Int, col: Int) {
+    def neighbours: Neighbours = {
+      Neighbours(Field(row, col - 1), Field(row, col + 1), Field(row + 1, col), Field(row - 1, col),
+        Field(row + 1, col +1), Field(row + 1, col - 1), Field(row -1, col + 1), Field(row - 1, col - 1))
+    }
+  }
+
+  import Fields._
+
+  private def part1(input: Fields): Int = {
+    computeFields(input, 100, List(input)).map(_.values.count(_ == 0)).sum
+  }
+
+  private def part2(input: Fields): Int = {
+    indexWhenAllFlash(input, 0, List(input))
+  }
+
+  val input = parseInput
+
+  println(s"Part 1: ${part1(input)}")
+  println(s"Part 2: ${part2(input)}")
+}
