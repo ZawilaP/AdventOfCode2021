d18ecbdd1ab4ffdf064ed3323fb9bda39f371a54
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2022-01-22 17:19:27 +0100

    Removing exercise
    Class was moved to Commons package

--- scala/Exercise.scala
+++
@@ -1,5 +0,0 @@
-abstract class Exercise extends App {
-  val day: String = getClass.getSimpleName.replaceAll("\\$", "")
-  val inputFileName: String = day + "_input.txt"
-  val path: String = "data/" + inputFileName
-}
