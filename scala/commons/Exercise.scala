e3a3af1df0fe62acd662cd4a8fdc416a46766273
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2022-01-22 17:19:03 +0100

    Adding commons package

---
+++ scala/commons/Exercise.scala
@@ -0,0 +1,7 @@
+package commons
+
+abstract class Exercise extends App {
+  val day: String = getClass.getSimpleName.replaceAll("\\$", "")
+  val inputFileName: String = day + "_input.txt"
+  val path: String = "data/" + inputFileName
+}
