e0eb24421217e9e1af0f2ae7e63679cb4b6ea5a6
Piotr Zawiła-Niedźwiecki <60758581+ZawilaP@users.noreply.github.com>
2022-01-24 16:50:59 +0100

    Cleaning of day2

--- scala/day2.scala
+++ scala/day2.scala
@@ -16,64 +16,52 @@ object day2 extends Exercise {
       case x if x.startsWith("up") => Up(x.last.asDigit)
     }
     source.close()
+
     input
   }
 
-  case class Distance(horizontal: Int, vertical: Int)
+  case class Distance(horizontal: Int, vertical: Int) {
+    def calculateTotalDistance: Int = horizontal * vertical
+  }
 
   sealed trait Directions
-
   case class Forward(length: Int) extends Directions
-
   case class Up(length: Int) extends Directions
-
   case class Down(length: Int) extends Directions
 
   private def calculatePosition(directions: Seq[Directions]): Distance = {
     @tailrec
     def helper(horizontal: Int, vertical: Int, acc: Seq[Directions]): Distance = {
       if (acc.isEmpty) Distance(horizontal, vertical)
-      else {
-        acc.head match {
+      else acc.head match {
           case Forward(x) => helper(horizontal + x, vertical, acc.tail)
           case Up(x) => helper(horizontal, vertical - x, acc.tail)
           case Down(x) => helper(horizontal, vertical + x, acc.tail)
         }
-      }
     }
 
     helper(0, 0, directions)
   }
 
-  def calculateTotalDistance(distance: Distance): Int = distance.horizontal * distance.vertical
-
-
   def calculateAimedPosition(directions: Seq[Directions]): Distance = {
     @tailrec
     def helper(horizontal: Int, vertical: Int, aim: Int, acc: Seq[Directions]): Distance = {
       if (acc.isEmpty) Distance(horizontal, vertical)
-      else {
-        acc.head match {
+      else acc.head match {
           case Forward(x) => helper(horizontal + x, vertical + aim * x, aim, acc.tail)
           case Up(x) => helper(horizontal, vertical, aim - x, acc.tail)
           case Down(x) => helper(horizontal, vertical, aim + x, acc.tail)
         }
-      }
     }
 
     helper(0, 0, 0, directions)
   }
 
-  def part1(lines: Seq[Directions]): Int = {
-    (calculatePosition _ andThen calculateTotalDistance) (lines)
-  }
-
-  def part2(lines: Seq[Directions]): Int = {
-    (calculateAimedPosition _ andThen calculateTotalDistance) (lines)
-  }
+  def part1(lines: Seq[Directions]): Int = calculatePosition(lines).calculateTotalDistance
+  def part2(lines: Seq[Directions]): Int = calculateAimedPosition(lines).calculateTotalDistance
 
   val parsedInput = parseInput
 
-  println(s"Part 1: ${timeIt {part1(parsedInput)}}")
-  println(s"Part 2: ${timeIt {part2(parsedInput)}}")
+  println(s"Part 1: ${timeIt {part1(parsedInput)}}") //1989265
+  println(s"Part 2: ${timeIt {part2(parsedInput)}}") //2089174012
 }
